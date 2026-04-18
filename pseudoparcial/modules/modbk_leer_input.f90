88a92e60cb93d7f1dffd619ea8b3c568ec734c62
Luis <luisfranconahuelquir@gmail.com>
2026-04-18 02:48:21 -0300

    Delete pseudoparcial directory

--- pseudoparcial/modules/modbk_leer_input.f90
+++
@@ -1,7 +0,0 @@
-module modbk_leer_input
-implicit none
-	
-	integer nrow ! numero de filas
-	integer ncol ! numero de columnas
-	
-end module modbk_leer_input
--- pseudoparcial/subrutinas/posiciones.f90
+++
@@ -1,9 +0,0 @@
-subroutine posiciones(n, o, h1, h2)
-use mis_subrutinas, only: crear_vectores
-implicit none
-	integer, intent(in) :: n
-	real(8), intent(out) :: o(n), h1(n), h2(n)
-	
-	call crear_vectores(trim("parcial/inputs/nose.dat"), n, o, h1, h2)
-
-end subroutine posiciones
